17e6f018708734990f4a4247e0502279dbf1a67f
ClaratZ <113831732+ClaratZ@users.noreply.github.com>
2022-09-21 03:36:31 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -2,5 +2,5 @@ import requests
 
 nomor = input("Nomor : ")
 jumlah = int(input("Jumlah : "))
-for i in range(jumlah)
+for i in range(jumlah):
       url = requests.get(f"https://darkteam.my.id/minispam/spamsms.php?nomor= ")
